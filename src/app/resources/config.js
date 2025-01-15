288153c846fd4339a2f901e92bcb403eb39d1287
iEatFishFingers <31018343+iEatFishFingers@users.noreply.github.com>
2025-01-15 19:16:33 +0000

    changed readme and change some config

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -4,14 +4,14 @@ const routes = {
     '/':        true,
     '/about':   true,
     '/work':    true,
-    '/blog':    true,
-    '/gallery': true,
+    '/blog':    false,
+    '/gallery': false,
 }
 
 // Enable password protection on selected routes
 // Set password in pages/api/authenticate.ts
 const protectedRoutes = {
-    '/work/automate-design-handovers-with-a-figma-to-code-pipeline': true
+    '/work/automate-design-handovers-with-a-figma-to-code-pipeline': false
 }
 
 const style = {
